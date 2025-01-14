d8d3839022715cad20fc46f9d419e2e16489c4ab
Artem Kozin <akozin@car9s1.42barcelona.com>
2025-01-14 17:25:25 +0100

    i might hate c++

--- src/RequestHeadParser.cpp
+++ src/RequestHeadParser.cpp
@@ -33,7 +33,8 @@ RequestHeadParser::RequestHeadParser(std::string r)
 	std::vector<std::string>::iterator	it = _acceptableMethods.begin();
 	while (it != _acceptableMethods.end())
 	{
-		if (std::find(_r.begin(), _r.end(), *it) == _r.begin())
+		// must set *it to char * ffs XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX 
+		if (_r.find(std*it, 0, 16) == 0)
 		{
 			_method = (*it).substr((*it).size() - 1);
 			break ;
@@ -55,7 +56,7 @@ RequestHeadParser::RequestHeadParser(std::string r)
 	}
 
 //	if (std::find(_r.end() - std::string("HTTP/1.1").size() - 1, _r.end(), "HTTP/1.1") == _r.end())
-	if (*(std::find(line.end() - std::string("HTTP/1.1\r").size() - 1, line.end(), "HTTP/1.1\r")) != "HTTP/1.1\r")
+	if (line.find("HTTP/1.1\r", line.size() - std::string("HTTP/1.1\r").size()) == std::string::npos)
 	{
 		throw badRequest();
 	}
